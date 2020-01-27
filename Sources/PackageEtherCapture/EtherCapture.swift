65060ec77fd284d48690a8057adbf2f32786dd1e
Darrell Root <darrellrootjunk@mac.com>
2020-01-26 20:56:04 -0800

    got rid of some unused code

--- Sources/PackageEtherCapture/EtherCapture.swift
+++ Sources/PackageEtherCapture/EtherCapture.swift
@@ -138,8 +138,6 @@ public class EtherCapture {
                         let timestamp = header.pointee.ts
                         let packetLength = header.pointee.len  //we may not capture whole packet
                         let captureLength = Int(header.pointee.caplen)
-                        debugPrint("packet ptr \(String(describing: ptr))")
-                            //self.packetCount = self.packetCount + 1
                         let data = Data(bytes: ptr, count: captureLength)
                         let frame = Frame(data: data, timeval: timestamp)
                         if let pointee = args?.pointee,                        Int(pointee) < EtherCapture.callbacks.count {
@@ -161,43 +159,8 @@ public class EtherCapture {
             callback(frame)
         }
     }*/
-    public func setCallback(_ callback: @escaping (Frame) -> Void) {
-        EtherCapture.callbacks.append(callback)
-        guard EtherCapture.callbacks.count < 256 else {
-            fatalError("TODO PackageEtherCapture can only handle 256 captures")
-        }
-        var callbackIndex: u_char = u_char(EtherCapture.callbacks.count - 1)
-        let callbackIndexPointer = UnsafeMutablePointer<u_char>.allocate(capacity: 1)
-        callbackIndexPointer.initialize(from: &callbackIndex, count: 1)
-        
-        DispatchQueue.global().async {
-            
-            
-            pcap_loop(self.pcap, 0,
-                {
-                    (args: UnsafeMutablePointer<UInt8>?,
-                     header:UnsafePointer<pcap_pkthdr>?,
-                     ptr: UnsafePointer<UInt8>?) -> () in
-                    if let header = header, let ptr = ptr {
-                        let timestamp = header.pointee.ts
-                        let packetLength = header.pointee.len  //we may not capture whole packet
-                        let captureLength = Int(header.pointee.caplen)
-                        debugPrint("packet ptr \(String(describing: ptr))")
-                            //self.packetCount = self.packetCount + 1
-                        let data = Data(bytes: ptr, count: captureLength)
-                        let frame = Frame(data: data, timeval: timestamp)
-                        if let pointee = args?.pointee,                        Int(pointee) < EtherCapture.callbacks.count {
-                            EtherCapture.callbacks[Int(pointee)](frame)
-                        } else {
-                            debugPrint("Invalid PackageEtherCapture callback")
-                        }
-                    }
-                                
-                },
-            callbackIndexPointer)
-
-        }
-    }
+    
+    /*old code from prior to switch to pcap_loop
     public func nextPacket() -> Frame? {
         
         var header = pcap_pkthdr()
@@ -221,5 +184,5 @@ public class EtherCapture {
         } else {
             return nil
         }
-    }
+    }*/
 }
