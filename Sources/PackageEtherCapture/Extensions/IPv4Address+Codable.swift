28a9df13f5dcb0a090d573934c9562052cc212a0
Darrell Root <darrellrootjunk@mac.com>
2020-02-01 08:19:33 -0800

    Back to debugDescription for codeable.  I like readable json.

--- Sources/PackageEtherCapture/Extensions/IPv4Address+Codable.swift
+++ Sources/PackageEtherCapture/Extensions/IPv4Address+Codable.swift
@@ -1,6 +1,6 @@
 //
 //  IPv4Address+Codable.swift
-//  
+//
 //
 //  Created by Darrell Root on 1/31/20.
 //
@@ -10,7 +10,7 @@ import Network
 
 extension IPv4Address: Codable {
     enum CodingKeys: String, CodingKey {
-        case ipv4Data
+        case ipv4String
     }
     enum IPv4AddressDecodingError: Error {
         case decoding(String)
@@ -18,14 +18,14 @@ extension IPv4Address: Codable {
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        let addressData = self.rawValue
-        try container.encode(addressData, forKey: .ipv4Data)
+        let addressString = self.debugDescription
+        try container.encode(addressString, forKey: .ipv4String)
     }
     
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        let addressData = try values.decode(Data.self, forKey: .ipv4Data)
-        guard let ipv4Address = IPv4Address(addressData) else {
+        let addressString = try values.decode(String.self, forKey: .ipv4String)
+        guard let ipv4Address = IPv4Address(addressString) else {
             throw IPv4AddressDecodingError.decoding("unable to decode IPv4 address from \(values)")
         }
         self = ipv4Address
--- Sources/PackageEtherCapture/Extensions/IPv6Address+Codable.swift
+++ Sources/PackageEtherCapture/Extensions/IPv6Address+Codable.swift
@@ -1,6 +1,6 @@
 //
 //  IPv6Address+Codable.swift
-//  
+//
 //
 //  Created by Darrell Root on 1/31/20.
 //
@@ -10,7 +10,7 @@ import Network
 
 extension IPv6Address: Codable {
     enum CodingKeys: String, CodingKey {
-        case ipv6Data
+        case ipv6String
     }
     enum IPv6AddressDecodingError: Error {
         case decoding(String)
@@ -18,14 +18,14 @@ extension IPv6Address: Codable {
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        let addressData = self.rawValue
-        try container.encode(addressData, forKey: .ipv6Data)
+        let addressString = self.debugDescription
+        try container.encode(addressString, forKey: .ipv6String)
     }
     
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        let addressData = try values.decode(Data.self, forKey: .ipv6Data)
-        guard let ipv6Address = IPv6Address(addressData) else {
+        let addressString = try values.decode(String.self, forKey: .ipv6String)
+        guard let ipv6Address = IPv6Address(addressString) else {
             throw IPv6AddressDecodingError.decoding("unable to decode IPv6 address from \(values)")
         }
         self = ipv6Address
