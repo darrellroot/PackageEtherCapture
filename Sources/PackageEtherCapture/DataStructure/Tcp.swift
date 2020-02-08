0d0fcda2b1bdabe2b2ee160d43c11cf71bcc1a21
Darrell Root <darrellrootjunk@mac.com>
2020-02-07 19:56:18 -0800

    public flags

--- Sources/PackageEtherCapture/DataStructure/Tcp.swift
+++ Sources/PackageEtherCapture/DataStructure/Tcp.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct Tcp: EtherDisplay, Codable {
-    var flags: String {
+    public var flags: String {
         var output = ""
         if self.syn {
             output.append("S")
